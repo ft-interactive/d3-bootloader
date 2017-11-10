0adb2702851a0644375417519684e4748fdb291f
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-11-10 16:18:10 +0000

    Adding save-svg-as-png

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ var defaultMap = {
   'g-xaxisdate': 'https://rawgit.com/ft-interactive/g-axis/master/build/g-axis.js',
   'g-yaxislinear': 'https://rawgit.com/ft-interactive/g-axis/master/build/g-axis.js',
   'd3-quarterly': 'https://rawgit.com/ft-interactive/d3-quarterly/master/index.js',
+  'save-svg-as-png': 'https://unpkg.com/save-svg-as-png@^1.2.0/saveSvgAsPng.js',
   '@financial-times/load-data': 'https://unpkg.com/@financial-times/load-data@^1.0.2/dist/loadData.dist.min.js',
 };
 
