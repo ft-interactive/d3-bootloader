321437f9f1b54e0f7a57c578aea8d350b50340cd
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-09-05 15:45:06 +0100

    Changing rawgit to unpkg; bumping

--- index.js
+++ index.js
@@ -12,8 +12,9 @@
 import SystemJS from 'systemjs';
 
 var defaultMap = {
-  '@financial-times/load-data': 'https://unpkg.com/@financial-times/load-data@^1.0.2/dist/loadData.dist.min.js',
-  'd3': 'https://d3js.org/d3.v4.js',
+  '@financial-times/load-data':
+    'https://unpkg.com/@financial-times/load-data@^1.0.2/dist/loadData.dist.min.js',
+  d3: 'https://d3js.org/d3.v4.js',
   'd3-array': 'https://d3js.org/d3.v4.js',
   'd3-axis': 'https://d3js.org/d3.v4.js',
   'd3-brush': 'https://d3js.org/d3.v4.js',
@@ -32,7 +33,7 @@ var defaultMap = {
   'd3-path': 'https://d3js.org/d3.v4.js',
   'd3-polygon': 'https://d3js.org/d3.v4.js',
   'd3-quadtree': 'https://d3js.org/d3.v4.js',
-  'd3-quarterly': 'https://rawgit.com/ft-interactive/d3-quarterly/master/index.js',
+  'd3-quarterly': 'https://rawgit.com/ft-interactive/d3-quarterly/v1.0.0/index.js',
   'd3-queue': 'https://d3js.org/d3.v4.js',
   'd3-random': 'https://d3js.org/d3.v4.js',
   'd3-request': 'https://d3js.org/d3.v4.js',
@@ -46,20 +47,20 @@ var defaultMap = {
   'd3-transition': 'https://d3js.org/d3.v4.js',
   'd3-voronoi': 'https://d3js.org/d3.v4.js',
   'd3-zoom': 'https://d3js.org/d3.v4.js',
-  'g-annotations': 'https://rawgit.com/ft-interactive/g-annotations/master/build/g-annotations.js',
-  'g-axis': 'https://rawgit.com/ft-interactive/g-axis/master/build/g-axis.js',
-  'g-chartcolour': 'https://rawgit.com/ft-interactive/g-chartcolour/master/build/g-chartcolour.js',
-  'g-chartframe': 'https://rawgit.com/ft-interactive/g-chartframe/master/build/g-chartframe.js',
-  'g-legend': 'https://rawgit.com/ft-interactive/g-legend/master/build/g-legend.js',
-  'g-xaxisdate': 'https://rawgit.com/ft-interactive/g-axis/master/build/g-axis.js',
-  'g-yaxislinear': 'https://rawgit.com/ft-interactive/g-axis/master/build/g-axis.js',
+  'g-annotations': 'https://unpkg.com/g-annotations@0.0.8',
+  'g-axis': 'https://unpkg.com/g-axis@1',
+  'g-chartcolour': 'https://unpkg.com/g-chartcolour@0.8',
+  'g-chartframe': 'https://unpkg.com/g-chartframe@5',
+  'g-legend': 'https://unpkg.com/g-legend@0.0.8',
+  'g-xaxisdate': 'https://unpkg.com/g-axis@1',
+  'g-yaxislinear': 'https://unpkg.com/g-axis@1',
   'plugin-babel': 'https://unpkg.com/systemjs-plugin-babel@0.0.25',
   'plugin-json': 'https://unpkg.com/systemjs-plugin-json@0.3.0',
-  'save-svg-as-png': 'https://unpkg.com/save-svg-as-png@^1.2.0/saveSvgAsPng.js',
-  'simple-statistics': 'https://unpkg.com/simple-statistics@^5.2.0',
-  'systemjs-babel-build': 'https://unpkg.com/systemjs-plugin-babel@0.0.25/systemjs-babel-browser.js',
+  'save-svg-as-png': 'https://unpkg.com/save-svg-as-png@1',
+  'simple-statistics': 'https://unpkg.com/simple-statistics@5',
+  'systemjs-babel-build':
+    'https://unpkg.com/systemjs-plugin-babel@0.0.25/systemjs-babel-browser.js',
   'topojson-client': 'https://unpkg.com/topojson-client@3',
-
 };
 
 var config = {
